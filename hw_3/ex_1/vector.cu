0156bc21e17a185fb12fdd7a460e9c3acb8c2eee
Felix Söderman <felixsoderman@gmail.com>
2023-09-12 12:44:49 +0200

    started on report and added timing

--- hw_3/ex_1/vector.cu
+++ hw_3/ex_1/vector.cu
@@ -6,16 +6,31 @@
 
 __global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len) {
   //@@ Insert code to implement vector addition here
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < len){
     out[i] = in1[i] + in2[i];
   }
 }
 
+
 //@@ Insert code to implement timer start
 
-//@@ Insert code to implement timer stop
+double cpuSecond() {
+   struct timeval tp;
+   gettimeofday(&tp,NULL);
+   return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
 
+static double timer;
+
+void timerStart() {
+  timer=cpuSecond();
+}
+
+//@@ Insert code to implement timer stop
+double timerStop() {
+    return cpuSecond() - timer;
+}
 
 int main(int argc, char **argv) {
   
@@ -32,53 +47,67 @@ int main(int argc, char **argv) {
   if(argc >= 2) {
     inputLength = atoi(argv[1]);
   }else{
-    printf("%s","Error, no length given");
+    printf("%s","Error, no length given\n");
+    return 0;
   } 
-    printf("The input length is %d\n", inputLength);
-  
+  //printf("The input length is %d\n", inputLength);
+  printf("%d, ",inputLength);
   //@@ Insert code below to allocate Host memory for input and output
-  hostInput1 = (double *)malloc(inputLength * sizeof(DataType));
-  hostInput2 = (double *)malloc(inputLength * sizeof(DataType));    
-  hostOutput = (double *)malloc(inputLength * sizeof(DataType));
-  resultRef = (double *)malloc(inputLength * sizeof(DataType));  
-  
+  //timerStart();
+  hostInput1 = (DataType *)malloc(inputLength * sizeof(DataType));
+  hostInput2 = (DataType *)malloc(inputLength * sizeof(DataType));    
+  hostOutput = (DataType *)malloc(inputLength * sizeof(DataType));
+  resultRef  = (DataType *)malloc(inputLength * sizeof(DataType));  
+  //printf("HostMalloc, cudaMalloc, cudaMemcpyHostToDevice, cudaRun, cudaMemcpyDeviceToHost\n");
+  //printf("%f, ",  timerStop());
+
   //@@ Insert code below to initialize hostInput1 and hostInput2 to random numbers, and create reference result in CPU
   srand(time(NULL));   // Initialization  
 
-  // You can scale the random double to any desired range
-    double max = 10000.0;
+  // You can scale the random DataType to any desired range
+    DataType max = 10000.0;
   
   for(int i = 0; i < inputLength; ++i){  
-    hostInput1[i] =  (double)rand() / RAND_MAX * max;
-    hostInput2[i] =  (double)rand() / RAND_MAX * max;
+    hostInput1[i] =  (DataType)rand() / RAND_MAX * max;
+    hostInput2[i] =  (DataType)rand() / RAND_MAX * max;
     resultRef[i] = hostInput1[i] + hostInput2[i];
   }
 
   //@@ Insert code below to allocate GPU memory here
-
+  //timerStart();
   cudaMalloc(&deviceInput1, inputLength * sizeof(DataType));
   cudaMalloc(&deviceInput2, inputLength * sizeof(DataType));  
   cudaMalloc(&deviceOutput, inputLength * sizeof(DataType));
-  
+  //printf("%f, ",  timerStop());
 
   //@@ Insert code to below to Copy memory to the GPU here
-  cudaMemcpy(deviceInput1,hostInput1,inputLength*sizeof(double),cudaMemcpyHostToDevice);
-  cudaMemcpy(deviceInput2,hostInput2,inputLength*sizeof(double),cudaMemcpyHostToDevice);
+  timerStart();
+
+  cudaMemcpy(deviceInput1,hostInput1,inputLength*sizeof(DataType),cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInput2,hostInput2,inputLength*sizeof(DataType),cudaMemcpyHostToDevice);
   
+  printf("%f, ",  timerStop());
+
   
   //@@ Initialize the 1D grid and block dimensions here
-  int Dg = TPB;
-  int Db = (inputLength+TPB-1)/TPB;
+  int Dg = (inputLength+TPB-1)/TPB;
+  int Db = TPB;
+
+  /*printf("Number of thread blocks: %d\n",Dg);
+  printf("Number of thread per block: %d\n",Db);*/
   
   //@@ Launch the GPU Kernel here
-  
-  vecAdd<<<Dg, Db>>>(deviceInput1,deviceInput2, deviceOutput, inputLength);
+  timerStart();
+  vecAdd<<<dim3(Dg,1,1), dim3(Db,1,1)>>>(deviceInput1,deviceInput2, deviceOutput, inputLength);
 
   cudaDeviceSynchronize();
+  printf("%f, ",  timerStop());
   //@@ Copy the GPU memory back to the CPU here
+  timerStart();
 
-  cudaMemcpy(hostOutput, deviceOutput, inputLength*sizeof(double), cudaMemcpyDeviceToHost);
-
+  cudaMemcpy(hostOutput, deviceOutput, inputLength*sizeof(DataType), cudaMemcpyDeviceToHost);
+  
+  printf("%f\n",  timerStop());
   //@@ Insert code below to compare the output with the reference
 
   bool diff = false;
@@ -91,7 +120,7 @@ int main(int argc, char **argv) {
   }
 
   if(!diff){
-    printf("Outputs are the same\n");
+    //printf("Outputs are the same\n");
   }
   
   //@@ Free the GPU memory here
---
+++ hw_3/report/report.md
@@ -0,0 +1,40 @@
+## Exercise 1 - Your first CUDA program and GPU performance metrics
+1. Explain how the program is compiled and run.
+
+    I have a make file in the code repo (I used nvcc)
+
+2. For a vector length of N:
+
+    1. How many floating operations are being performed in your vector add kernel? 
+        
+        When adding a two vectors of length N there are N plus floating point operations that are performed.
+
+    2. How many global memory reads are being performed by your kernel? 
+        
+        Since both vectors are read once for each addition there is a total of 2N globalreads from global memory.
+
+3. For a vector length of 1024:
+    1. Explain how many CUDA threads and thread blocks you used. 
+
+        I used (1024+32-1)/32 = 32 thread blocks.
+        and I used 32*32=1024 CUDA threads.
+        
+
+    2. Profile your program with Nvidia Nsight. What Achieved Occupancy did you get? You might find https://docs.nvidia.com/nsight-compute/NsightComputeCli/index.html#nvprof-metric-comparisonLinks to an external site. useful.
+
+
+
+4. Now increase the vector length to 131070:
+
+    1. Did your program still work? If not, what changes did you make?
+
+        The program still works. No changes needed to be made.
+
+    2. Explain how many CUDA threads and thread blocks you used.
+
+        I used (131070+32-1)/32 = 4096 thread blocks.
+        and I used 4096*32 = 131072 CUDA threads.
+
+    3. Profile your program with Nvidia Nsight. What Achieved Occupancy do you get now?
+
+5. Further increase the vector length (try 6-10 different vector length), plot a stacked bar chart showing the breakdown of time including (1) data copy from host to device (2) the CUDA kernel (3) data copy from device to host. For this, you will need to add simple CPU timers to your code regions.
