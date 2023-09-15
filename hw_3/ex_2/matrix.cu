6de96b9521d619e7a5dab6aa7ba82b9e364ee49a
Felix Söderman <felixsoderman@gmail.com>
2023-09-15 09:39:10 +0200

    Fixed report add copied code from colab

---
+++ hw_3/ex_2/matrix.cu
@@ -0,0 +1,234 @@
+%%writefile matrix.cu
+
+#include <stdio.h>
+#include <sys/time.h>
+
+#include<unistd.h>
+
+
+#define DataType double
+#define TPB 32 //32 is max since 32*32 = 1024
+
+// Compute C = A * B
+
+/*
+1 2  4 3    1*4 + 2*2    3*1 + 2*1      4 + 4   3 + 2
+3 4  2 1  = 3*4 + 4*2    3*3 + 4*1 =    12 + 8  9 + 4
+*/
+
+//@@ Insert code to implement matrix multiplication here
+
+__global__ void gemmShared(DataType *A, DataType *B, DataType *C, int numARows,
+                      int numAColumns, int numBRows, int numBColumns){
+  extern __shared__ DataType out[];
+
+  out[threadIdx.x] = A[blockIdx.x*numAColumns + threadIdx.x] * B[threadIdx.x*numBColumns+blockIdx.y];
+  __syncthreads();
+
+  if(threadIdx.x == 0){
+    // Next line not needed whem memset is used before launching the kernal
+    // C[blockIdx.x*numBColumns+blockIdx.y] = 0;
+    for(int k = 0; k < numAColumns; ++k){
+      atomicAdd(&C[blockIdx.x*numBColumns+blockIdx.y],out[k]);
+      //C[blockIdx.x*numBColumns+blockIdx.y] += out[k];
+    }
+  }
+}
+
+// REQUIRES MEMSET to 0
+__global__ void gemmAtomicAdd(DataType *A, DataType *B, DataType *C, int numARows,
+                      int numAColumns, int numBRows, int numBColumns){
+  atomicAdd(&C[blockIdx.x*numBColumns+blockIdx.y],A[blockIdx.x*numAColumns + threadIdx.x] * B[threadIdx.x*numBColumns+blockIdx.y]);
+}
+
+__global__ void gemmBIG(DataType *A, DataType *B, DataType *C, int numARows,
+                      int numAColumns, int numBRows, int numBColumns){
+  //@@ Insert code to implement matrix multiplication here
+
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if(i < numARows && j < numBColumns){
+    for(int k=0;k<numAColumns;++k){
+      C[i*numBColumns+j] += A[i*numAColumns + k] * B[k*numBColumns+j];
+    }
+  }
+}
+
+//@@ Insert code to implement timer stop
+
+static double timer;
+
+double cpuSecond() {
+   struct timeval tp;
+   gettimeofday(&tp,NULL);
+   return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
+
+void timerStart() {
+  timer=cpuSecond();
+}
+
+double timerStop() {
+    return cpuSecond() - timer;
+}
+
+
+int main(int argc, char **argv) {
+
+  DataType *hostA; // The A matrix
+  DataType *hostB; // The B matrix
+  DataType *hostC; // The output C matrix
+  DataType *resultRef; // The reference result
+  DataType *deviceA;
+  DataType *deviceB;
+  DataType *deviceC;
+  int numARows;    // number of rows in the matrix A
+  int numAColumns; // number of columns in the matrix A
+  int numBRows;    // number of rows in the matrix B
+  int numBColumns; // number of columns in the matrix B
+  int numCRows;
+  int numCColumns;
+
+  int method = 2;
+
+  //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+  if(argc >= 4) {
+      numARows = atoi(argv[1]);
+      numAColumns = atoi(argv[2]);
+      numBColumns = atoi(argv[3]);
+      numBRows = numAColumns;
+      numCRows = numARows;
+      numCColumns = numBColumns;
+      if(argc >= 5){
+        method = atoi(argv[4]);
+        if((method == 0 or method == 1) && (numAColumns > 1024)){
+          printf("Method does not support that large matrices");
+          return -1;
+        }
+      }
+  }else{
+      printf("%s","Error, please enter numARows, numAColumns, numBColumns\n");
+      return 0;
+  }
+  //printf("%s","Error, please enter numARows, numAColumns, numBColumns\n");
+  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
+
+  //@@ Insert code below to allocate Host memory for input and output
+  hostA = (DataType *)malloc(numARows*numAColumns*sizeof(DataType));
+  hostB = (DataType *)malloc(numBRows*numBColumns*sizeof(DataType));
+  hostC = (DataType *)malloc(numCRows*numCColumns*sizeof(DataType));
+  resultRef = (DataType *)malloc(numCRows*numCColumns*sizeof(DataType));
+  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+
+  srand(time(NULL));   // Initialization
+
+  DataType max = 10000;
+
+  for(int i = 0; i < numARows*numAColumns; ++i){
+    hostA[i] =  (DataType)rand() / RAND_MAX * max;
+  }
+
+  for(int i = 0; i < numBRows*numBColumns; ++i){
+    hostB[i] =  (DataType)rand() / RAND_MAX * max;
+  }
+
+  for(int i = 0; i < numCRows*numCColumns; ++i){
+    int row = i/numCColumns;
+    int col = i%numCColumns;
+    resultRef[i] = 0;
+    for(int k=0;k<numAColumns;++k){
+      resultRef[i] += hostA[row*numAColumns+k] * hostB[k*numBColumns+col];
+    }
+  }
+
+  //memset(hostC,0,numCRows*numCColumns*sizeof(DataType));
+
+  //@@ Insert code below to allocate GPU memory here
+
+  cudaMalloc(&deviceA, numAColumns*numARows * sizeof(DataType));
+  cudaMalloc(&deviceB, numBColumns*numBRows * sizeof(DataType));
+  cudaMalloc(&deviceC, numCColumns*numCRows * sizeof(DataType));
+
+  //@@ Insert code to below to Copy memory to the GPU here
+  timerStart();
+  cudaMemcpy(deviceA,hostA,numAColumns*numARows*sizeof(DataType),cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceB,hostB,numBColumns*numBRows*sizeof(DataType),cudaMemcpyHostToDevice);
+  printf("cudaMemcpyHostToDevice : %f \n",timerStop());
+
+  //@@ Set deviceMemC to 0
+  cudaMemset(deviceC,0,numCRows*numCColumns*sizeof(DataType));
+
+  //@@ Initialize the grid and block dimensions here
+
+  if (method == 0){
+      int Dgx = numCRows;
+      int Dgy = numCColumns;
+      int Db = numAColumns; // Does it have to be a factor of 32?
+
+      //@@ Launch the GPU Kernel here
+      timerStart();
+      gemmShared <<< dim3(Dgx,Dgy), dim3(Db), numAColumns*sizeof(DataType)>>>(deviceA, deviceB, deviceC,numARows,numAColumns, numBRows, numBColumns);
+      cudaDeviceSynchronize();
+      printf("First method (no atomic) took: %f \n",timerStop());
+  } else if (method == 1){
+      int Dgx = numCRows;
+      int Dgy = numCColumns;
+      int Dbx = numAColumns; // Does it have to be a factor of 32?
+      timerStart();
+      gemmAtomicAdd <<< dim3(Dgx,Dgy), dim3(Dbx)>>>(deviceA, deviceB, deviceC,numARows,numAColumns, numBRows, numBColumns);
+      cudaDeviceSynchronize();
+      printf("First method (atmoic) took: %f \n",timerStop());
+  } else if (method == 2){
+      int Dgx = (numCRows+TPB-1)/TPB;
+      int Dgy = (numCColumns+TPB-1)/TPB;
+      int Dbx = TPB; // Does it have to be a factor of 32?
+      int Dby = TPB;  // Does it have to be a factor of 32?
+      //@@ Launch the GPU Kernel here
+      timerStart();
+      gemmBIG <<< dim3(Dgx,Dgy), dim3(Dbx,Dby)>>>(deviceA,deviceB, deviceC,numARows,numAColumns, numBRows, numBColumns);
+      cudaDeviceSynchronize();
+      printf("Second method took: %f \n",timerStop());
+  }
+  else{
+    printf("Method not defined!");
+    return -1;
+  }
+
+  //@@ Copy the GPU memory back to the CPU here
+  timerStart();
+  cudaMemcpy(hostC, deviceC, numCRows*numCColumns*sizeof(DataType), cudaMemcpyDeviceToHost);
+  printf("cudaMemcpyDeviceToHost : %f \n",timerStop());
+
+  //@@ Insert code below to compare the output with the reference
+  bool diff = false;
+    for(int i = 0; i < numCRows*numCColumns;++i){
+      if((abs(resultRef[i] - hostC[i])) > 0.001){
+        printf("Not equal diffs at %d\n",i);
+        printf("HOST IS: %f, DEVICE IS: %f\n", resultRef[i], hostC[i]);
+        diff = true;
+      }
+    }
+
+  if(!diff){
+    printf("Outputs are the same\n");
+  }
+
+  //@@ Free the GPU memory here
+
+  cudaFree(deviceA);
+  cudaFree(deviceB);
+  cudaFree(deviceC);
+
+  //@@ Free the CPU memory here
+  free(hostA);
+  free(hostB);
+  free(hostC);
+  free(resultRef);
+
+  /*timerStart();
+  sleep(5);
+  printf("Timer test: %f \n",timerStop());*/
+
+  return 0;
+}
--- hw_3/report/report.md
+++ hw_3/report/report.md
@@ -45,3 +45,91 @@
 
 5. Further increase the vector length (try 6-10 different vector length), plot a stacked bar chart showing the breakdown of time including (1) data copy from host to device (2) the CUDA kernel (3) data copy from device to host. For this, you will need to add simple CPU timers to your code regions.
 
+    ![VectorAdd](/report/VectorAdd.svg)
+
+
+## Exercise 2 - Your first CUDA program and GPU performance metrics
+
+1. Name three applications domains of matrix multiplication.
+
+    One application is machine learning matrix multiplication is the base of training a model. This is why, tensor cores are developed esecially to calculate matrix multiplications.
+
+    Another application is solving linear equation systems. That is essential is engineering as many systems can be modeled as a linear combination of variables.
+
+    Lastly in game programming matrix multiplication can be used to rotate objects in 3d space (using rotation matrices).
+
+2.  How many floating operations are being performed in your matrix multiply kernel? 
+
+    I have written three different implementation of my kernal. The one using atomicAdd runs 2 floating point operation (per thread), one for multiplying the values and one for atomicadd to the C vector.
+
+    While the second runs first a two floating point operation (similar to the last one, but add to shared variable instead of atomicAdd), but if it is threadIdx.x = 0 it also does numAColumns additional floating point operations when taking the sum over the shared variable. 
+
+    Lastly my last kernal always runs numAColumns floating point operations (per thread).
+
+    When talking about an entire run we multiply the number from the previous question with how many threads are created. Which is equal to the amount of numCRows * numCColumns for the first and third kernal implementation but for the second one there is numCRows * numCColumns * numAColumns since it runs all calculations in paralell. 
+
+    (I added a table at the end of question 3 to show all values)
+
+
+3. How many global memory reads are being performed by your kernel?
+
+    All three versions of my kernals only read the global memory twice.
+
+    | Kernal                                                                                                     	| gemmShared                                                  	| gemmAtomicAdd                            	| gemmBIG                                  	|
+    |------------------------------------------------------------------------------------------------------------	|-------------------------------------------------------------	|------------------------------------------	|------------------------------------------	|
+    | Number of threads (excluding CUDA threads that do no global memory reads and no floating point operations) 	| numCColumns * numCRows * numAColumns                        	| numCColumns * numCRows * numAColumns     	| numCColumns * numCRows                   	|
+    | Number of floating point operations (per thread)                                                           	| 2 or 2+numAColumns                                          	| 2                                        	| 2*numAColumns                            	|
+    | Global memory reads (per thread)                                                                           	| 2                                                           	| 2                                        	| 2*numAColumns                            	|
+    | Number of floating point operations (total)                                                                	| (2 or 2+numAColumns) * numCColumns * numCRows * numAColumns 	| 2 * numCColumns * numCRows * numAColumns 	| 2 * numAColumns * numCColumns * numCRows 	|
+    | Global memory reads (total)                                                                                	| 2 * numCColumns * numCRows * numAColumns                    	| 2 * numCColumns * numCRows * numAColumns 	| 2 * numAColumns * numCColumns * numCRows     	|
+
+4. For a matrix A of (128x128) and B of (128x128):
+        
+    1. Explain how many CUDA threads and thread blocks you used.
+
+        For the first and second kernal implementation there is numCRows * numCColumns * numAColumns which results in 128^3 = 2097152 CUDA threads
+
+        For the third kernal implementation there are numCRows * numCColumns kernals which means that in this case it is 128^2 = 16384 CUDA threads.
+        
+    2. Profile your program with Nvidia Nsight. What Achieved Occupancy did you get? 
+
+    | Kernal                 	| gemmShared 	| gemmAtomicAdd 	| gemmBIG 	|
+    |------------------------	|------------	|---------------	|---------	|
+    | Cuda threads           	| 2097152      	| 2097152       	| 16384   	|
+    | Achieved occupancy (%) 	| 88.35      	| 88.55         	| 98.15   	|
+
+5. For a matrix A of (511x1023) and B of (1023x4094):
+    
+    1. Did your program still work? If not, what changes did you make?
+        This workes for all three of my kernal versions (first and second kernal does not allow for 1023 to go above 1024, since that would need more than 1024 threads per block which is not possible).
+
+    2. Explain how many CUDA threads and thread blocks you used.
+
+        For the first and second kernal implementation there is numCRows * numCColumns * numAColumns which results in 511 * 4094 * 1023  = 2140150782 CUDA threads
+
+        For the third kernal implementation there are numCRows * numCColumns kernals which means that in this case it is 511 * 4094 = 2092034 CUDA threads.
+
+    3. Profile your program with Nvidia Nsight. What Achieved Occupancy do you get now?
+
+        | Kernal                 	| gemmShared 	| gemmAtomicAdd 	| gemmBIG 	|
+        |------------------------	|------------	|---------------	|---------	|
+        | Cuda threads           	| 2140150782 	| 2140150782    	| 2092034 	|
+        | Achieved occupancy (%) 	| 16.33      	| 74.01         	| 98.64   	|
+
+6. Further increase the size of matrix A and B, plot a stacked bar chart showing the breakdown of time including (1) data copy from host to device (2) the CUDA kernel (3) data copy from device to host. For this, you will need to add simple CPU timers to your code regions. Explain what you observe.
+    In the plot one can see that the kernal time is by far the biggest contributing factor to the total time spent running the program. One can also see that the kernal called gemmBIG is the fastest. I assume this is becuase it has a order of magnitude less kernals (but each kernal does more work). This indicates that the overhead of creating kernals is substantial compared to the work done in gemmShared and gemmAtomicAdd.
+
+    ![Double gemmShared](/report/Double%20gemmShared.svg)
+    ![Double gemmAtomicAdd](/report/Double%20gemmAtomicAdd.svg)
+    ![Double gemmBIG](/report/Double%20gemmBIG.svg)
+
+
+
+7. Now, change DataType from double to float, re-plot the a stacked bar chart showing the time breakdown. Explain what you observe. 
+
+    Firstly I had to decrese the max random number from 10000 to 100 and increse the tolerane from 0.004 to 1. This is because otherwise the float was not able to store the result to high enough precision.
+
+
+    ![Float gemmShared](/report/Float%20gemmShared.svg)
+    ![Float gemmAtomicAdd](/report/Float%20gemmAtomicAdd.svg)
+    ![Float gemmBIG](/report/Float%20gemmBIG.svg)
